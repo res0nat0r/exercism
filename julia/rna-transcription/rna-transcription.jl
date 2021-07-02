aeb43138ce722e43dd7287e067509be01234b54c
Stefhen Hovland <stefhen.hovland@gmail.com>
2021-07-01 21:49:56 -0500

    julia/rna-transcription

---
+++ julia/rna-transcription/rna-transcription.jl
@@ -0,0 +1,27 @@
+"""
+Given a DNA strand, return its RNA complement (per RNA transcription).
+Both DNA and RNA strands are a sequence of nucleotides.
+The four nucleotides found in DNA are adenine (A), cytosine (C), guanine (G) and thymine (T).
+The four nucleotides found in RNA are adenine (A), cytosine (C), guanine (G) and uracil (U).
+Given a DNA strand, its transcribed RNA strand is formed by replacing each nucleotide with its complement:
+
+G -> C
+C -> G
+T -> A
+A -> U
+"""
+
+function to_rna(dna)
+  transcribed = Dict('G' => 'C', 'C' => 'G', 'T' => 'A', 'A' => 'U')
+  rna = ""
+
+  for i in 1:length(dna)
+    try
+      rna = string(rna, transcribed[dna[i]])
+    catch
+      throw(ErrorException("Invalid input"))
+    end
+  end
+
+  return rna
+end
