dfb2b937de62184a4fc3930790859f59d2fd2775
Stefhen Hovland <stefhen.hovland@gmail.com>
2021-07-01 21:35:33 -0500

    julia/hamming

---
+++ julia/hamming/hamming.jl
@@ -0,0 +1,16 @@
+"Your optional docstring here"
+function distance(a, b)
+  hamming = 0
+
+  if length(a) != length(b)
+    throw(ArgumentError("Invalid length"))
+  end
+
+  for i in 1:length(a)
+    if a[i] != b[i]
+      hamming += 1
+    end
+  end
+
+  return hamming
+end
