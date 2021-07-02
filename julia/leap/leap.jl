6fb984335260e89cc6725a20e8c9f3ff569128f6
Stefhen Hovland <stefhen.hovland@gmail.com>
2021-07-01 19:31:36 -0500

    julia/leap

---
+++ julia/leap/leap.jl
@@ -0,0 +1,24 @@
+"""
+    is_leap_year(year)
+
+Return `true` if `year` is a leap year in the gregorian calendar.
+
+on every year that is evenly divisible by 4
+  except every year that is evenly divisible by 100
+    unless the year is also evenly divisible by 400
+"""
+function is_leap_year(year)
+  if mod(year, 4) == 0 
+    if mod(year, 100) == 0
+      if mod(year, 400) == 0
+        return true
+      else
+        return false
+      end
+    else
+      return true
+    end
+  else
+    return false
+  end
+end
