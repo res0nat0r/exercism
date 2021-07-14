ffdff32aa471bddce0469fdc04a762c40f0067ad
Stefhen Hovland <stefhen.hovland@gmail.com>
2021-07-13 23:15:48 -0500

    wip

--- julia/pascals-triangle/pascals-triangle.jl
+++ julia/pascals-triangle/pascals-triangle.jl
@@ -1,10 +1,14 @@
 function triangle(n)
   pascal = []
 
-  for row in 0:n
-    for col in 0:n
-      append!(pascal, [row, col])
+  for line in 0:(n-1)
+    internal = []
+    for row in 0:(n-1)
+      for col in 0:(n-1)
+        append!(internal, binomial(row, col))
+      end
     end
+    append!(pascal, internal)
   end
 
   return pascal
