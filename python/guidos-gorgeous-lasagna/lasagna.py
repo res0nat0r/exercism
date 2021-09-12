f9495f416d48540d74e91e99330f85fdbd5a4be1
Stefhen Hovland <stefhen.hovland@gmail.com>
2021-09-12 18:10:53 -0500

    python/guidos-gorgeous-lasagna

---
+++ python/guidos-gorgeous-lasagna/lasagna.py
@@ -0,0 +1,38 @@
+"""
+Calculate how long it takes to bake a cake
+"""
+
+EXPECTED_BAKE_TIME = 40
+
+def bake_time_remaining(elapsed_bake_time):
+    """Calculate the bake time remaining.
+
+    :param elapsed_bake_time: int baking time already elapsed.
+    :return: int remaining bake time derived from 'EXPECTED_BAKE_TIME'.
+
+    Function that takes the actual minutes the lasagna has been in the oven as
+    an argument and returns how many minutes the lasagna still needs to bake
+    based on the `EXPECTED_BAKE_TIME`.
+    """
+
+    return EXPECTED_BAKE_TIME - elapsed_bake_time
+
+
+def preparation_time_in_minutes(layers):
+    """Calculate prep time
+
+    :parm layers: Number of layers in the cake
+    :return: int Total number of minutes to prepare the cake
+    """
+
+    return 2 * layers
+
+def elapsed_time_in_minutes(number_of_layers, elapsed_bake_time):
+    """Calculate total prep time
+
+    :parm number_of_layers: Number of layers in the cake
+    :parm elapsed_bake_time: Total amount of time spent prepping the cake
+    :retrun: int Total elapsed time
+    """
+
+    return preparation_time_in_minutes(number_of_layers) + elapsed_bake_time
