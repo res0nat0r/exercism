25d6f3dc6691bfd3063551d356bb063c6ce6895f
Stefhen Hovland <stefhen.hovland@gmail.com>
2022-07-05 20:21:17 +0000

    python/two-fer

---
+++ python/two-fer/two_fer.py
@@ -0,0 +1,10 @@
+def two_fer(name="you"):
+    """
+
+    :parm name: str name
+    :return: str of response
+
+    This takes a `name` as a parameter and returns a string
+    """
+
+    return f"One for {name}, one for me."
