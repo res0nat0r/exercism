1b284970df4cf3f2c5fd1ea553b8b528c89fdf28
Stefhen Hovland <stefhen.hovland@gmail.com>
2022-07-06 14:18:29 -0500

    python/triangle (#18)

---
+++ python/triangle/triangle.py
@@ -0,0 +1,39 @@
+def equilateral(sides):
+    (a, b, c) = sides
+
+    if is_triangle(sides) and (a == b == c):
+        return True
+
+    return False
+
+
+def isosceles(sides):
+    (a, b, c) = sides
+
+    if is_triangle(sides) and ((a == b) or (b == c) or (a == c)):
+        return True
+
+    return False
+
+
+def scalene(sides):
+    (a, b, c) = sides
+
+    if is_triangle(sides) and (a != b != c):
+        return True
+
+    return False
+
+
+def is_triangle(sides):
+    (a, b, c) = sides
+
+    if (
+        (a + b >= c)
+        and (b + c >= a)
+        and (a + c >= b)
+        and ((a > 0) and (b > 0) and (c > 0))
+    ):
+        return True
+
+    return False
