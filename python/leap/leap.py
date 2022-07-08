6f4da88101e428564942d8b983ccf6c74bfdbc21
Stefhen Hovland <stefhen.hovland@gmail.com>
2022-07-08 01:03:12 -0500

    python/leap (#29)

---
+++ python/leap/leap.py
@@ -0,0 +1,15 @@
+"""
+Determine the if the year is a leap year
+"""
+
+
+def leap_year(year):
+    """
+    :param year: int Year to test
+    :return: bool
+    """
+
+    if ((year % 4 == 0) and (year % 100 != 0)) or (year % 400 == 0):
+        return True
+
+    return False
