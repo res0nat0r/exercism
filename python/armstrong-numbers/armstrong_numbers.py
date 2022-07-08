63e50faf0fc982e944aef236460890d143437a49
Stefhen Hovland <stefhen.hovland@gmail.com>
2022-07-08 06:25:42 +0000

    working

---
+++ python/armstrong-numbers/armstrong_numbers.py
@@ -0,0 +1,16 @@
+"""
+An Armstrong number is a number that is the sum of its own digits each raised to the power of the number of digits.
+"""
+
+
+def is_armstrong_number(number):
+    total = 0
+    digits = len([d for d in str(number)])
+
+    for i in range(digits):
+        total += pow(int(str(number)[i]), digits)
+
+    if total == number:
+        return True
+
+    return False
