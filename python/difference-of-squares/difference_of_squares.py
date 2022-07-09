7dce25aac8e96919fe4b44e35aa5dbe652b9ce04
Stefhen Hovland <stefhen.hovland@gmail.com>
2022-07-09 02:21:23 +0000

    pylint

--- python/difference-of-squares/difference_of_squares.py
+++ python/difference-of-squares/difference_of_squares.py
@@ -1,11 +1,31 @@
+"""
+Find the difference between the square of the sum and the
+sum of the squares of the first N natural numbers.
+"""
+
+
 def square_of_sum(number):
+    """
+    :param number: int number to calculate
+    :return: int calculated value
+    """
+
     total = sum(range(1, number + 1))
     return total * total
 
 
 def sum_of_squares(number):
-    return sum([x * x for x in range(1, number + 1)])
+    """
+    :param number: int number to calculate
+    :return: int calculated value
+    """
+    return sum(x * x for x in range(1, number + 1))
 
 
 def difference_of_squares(number):
+    """
+    :param number: int number to calculate
+    :return: int calculated value
+    """
+
     return square_of_sum(number) - sum_of_squares(number)
