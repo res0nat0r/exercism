b2dfcfa9ea25cc129bb4b53e3ecb5e3b9c947127
Stefhen Hovland <stefhen.hovland@gmail.com>
2022-07-09 03:22:31 +0000

    wip

--- python/collatz-conjecture/collatz_conjecture.py
+++ python/collatz-conjecture/collatz_conjecture.py
@@ -1,2 +1,30 @@
+"""
+The Collatz Conjecture or 3x+1 problem can be summarized as follows:
+
+Take any positive integer n. If n is even, divide n by 2 to get n / 2. If n is
+odd, multiply n by 3 and add 1 to get 3n + 1. Repeat the process indefinitely.
+The conjecture states that no matter which number you start with, you will
+always reach 1 eventually.
+
+Given a number n, return the number of steps required to reach 1.
+"""
+
+
 def steps(number):
-    pass
+    counter = 0
+
+    while number > 0:
+        if is_even(number):
+            number = number / 2
+        else:
+            number = (number * 3) + 1
+    counter += 1
+
+    return counter
+
+
+def is_even(number):
+    if number % 2 == 0:
+        return True
+
+    return False
