8495237a630371f57426a4cebe807041c0e082bf
Stefhen Hovland <stefhen.hovland@gmail.com>
2022-07-12 15:27:57 -0500

    wip

--- python/yacht/yacht.py
+++ python/yacht/yacht.py
@@ -7,12 +7,12 @@ THREES = lambda dice: dice.count(3) * 3
 FOURS = lambda dice: dice.count(4) * 4
 FIVES = lambda dice: dice.count(5) * 5
 SIXES = lambda dice: dice.count(6) * 6
-FULL_HOUSE = lambda full_house: sum(full_house) if [2,3]==[full_house.count(i) for i in set(full_house)] else 0
+FULL_HOUSE = lambda full_house: sum(full_house) if [2, 3] == [full_house.count(i) for i in set(full_house)] else 0
 FOUR_OF_A_KIND = None
-LITTLE_STRAIGHT = None
-BIG_STRAIGHT = None
-CHOICE = None
+LITTLE_STRAIGHT = lambda little_straight: 30 if [1,2,3,4,5] == sorted(little_straight) else 0
+BIG_STRAIGHT = lambda big_straight: 30 if [2,3,4,5,6] == sorted(big_straight) else 0
+CHOICE = lambda choice: sum(choice)
 
 
 def score(dice, category):
-    return category(dice)    
+    return category(dice)
