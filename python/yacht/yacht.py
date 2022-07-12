0d48a994e0d547282543e66e9b405278dabd045d
Stefhen Hovland <stefhen.hovland@gmail.com>
2022-07-12 13:10:17 -0500

    full-house

--- python/yacht/yacht.py
+++ python/yacht/yacht.py
@@ -7,7 +7,7 @@ THREES = lambda dice: dice.count(3) * 3
 FOURS = lambda dice: dice.count(4) * 4
 FIVES = lambda dice: dice.count(5) * 5
 SIXES = lambda dice: dice.count(6) * 6
-FULL_HOUSE = None
+FULL_HOUSE = lambda full_house: sum(full_house) if [2,3]==[full_house.count(i) for i in set(full_house)] else 0
 FOUR_OF_A_KIND = None
 LITTLE_STRAIGHT = None
 BIG_STRAIGHT = None
