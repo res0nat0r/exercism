398107e73f06af9c7f6c248b7ec2368fd41ebf34
Stefhen Hovland <stefhen.hovland@gmail.com>
2022-07-13 05:11:32 +0000

    working

--- python/diffie-hellman/diffie_hellman.py
+++ python/diffie-hellman/diffie_hellman.py
@@ -1,10 +1,44 @@
+"""
+Alice and Bob use Diffie-Hellman key exchange to share secrets. They start with
+prime numbers, pick private keys, generate and share public keys, and then
+generate a shared secret key.
+"""
+
+from random import randint
+
+
 def private_key(p):
-    pass
+    """
+    Return the private key
+
+    :param int p: private key
+    :return: private key
+    """
+
+    return randint(1, p - 1)
 
 
 def public_key(p, g, private):
-    pass
+    """
+    Return the public key
+
+    :param int p: public key
+    :param int g: power
+    :param int private: private key
+    :return: public key
+    """
+
+    return pow(g, private) % p
 
 
 def secret(p, public, private):
-    pass
+    """
+    Return the secret key
+
+    :param int p: public key
+    :param int public: public key
+    :param int private: private key
+    :return: secret
+    """
+
+    return pow(public, private) % p
