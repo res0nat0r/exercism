89811a1251de31994827e97b22789331b0a8f38c
Stefhen Hovland <stefhen.hovland@gmail.com>
2022-07-22 23:07:37 +0000

    python/gigasecond

--- python/gigasecond/gigasecond.py
+++ python/gigasecond/gigasecond.py
@@ -1,2 +1,13 @@
+"""
+Given a moment, determine the moment that would be after a gigasecond has passed.
+"""
+
+from datetime import timedelta
+
+
 def add(moment):
-    pass
+    """
+    :param moment: datetime Time to offset
+    :return: datetime moment plus gigasecond
+    """
+    return moment + timedelta(seconds=1_000_000_000)
