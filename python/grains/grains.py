6e3ce63efe7bff2ad839b778f17d128d2559e2aa
Stefhen Hovland <stefhen.hovland@gmail.com>
2022-07-26 20:05:05 +0000

    wip

--- python/grains/grains.py
+++ python/grains/grains.py
@@ -1,7 +1,8 @@
 def square(number):
-    if number not in range(1,65):
+    if number not in range(1, 65):
         raise ValueError("square must be between 1 and 64")
 
+    return pow(2, number)
 
 
 def total():
