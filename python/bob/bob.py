32bf42e04bcb0da09ebfa052d221731e47722974
Stefhen Hovland <stefhen.hovland@gmail.com>
2022-10-15 22:31:47 -0400

    wip

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,8 +1,15 @@
 def response(hey_bob):
     hey_bob = hey_bob.strip()
 
-    if any(c.isalpha() for c in hey_bob):
+
+    if not any(c.isalpha() for c in hey_bob):
         return "Fine. Be that way!" 
+    
+    if hey_bob == '':
+        return "Whatever."
+    
+    if hey_bob.upper() == hey_bob and hey_bob[-1] == '?':
+        return "Calm down, I know what I'm doing!"
 
     if hey_bob[-1] == '?':
         return "Sure."
@@ -10,7 +17,3 @@ def response(hey_bob):
     if hey_bob.upper() == hey_bob:
         return "Whoa, chill out!"
 
-    if hey_bob.upper() == hey_bob and hey_bob[-1] == '?':
-        return "Calm down, I know what I'm doing!"
-
-    return "Whatever."
