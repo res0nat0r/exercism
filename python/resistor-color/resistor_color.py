e4c569daf464196c7e58a3079417eea750f7a249
Stefhen Hovland <stefhen.hovland@gmail.com>
2022-10-15 23:21:00 -0400

    working

--- python/resistor-color/resistor_color.py
+++ python/resistor-color/resistor_color.py
@@ -1,6 +1,39 @@
+"""Resistor colors"""
+
+
 def color_code(color):
-    pass
+    """Color codes"""
+
+    lookup = [
+        "black",
+        "brown",
+        "red",
+        "orange",
+        "yellow",
+        "green",
+        "blue",
+        "violet",
+        "grey",
+        "white",
+    ]
+
+    for i in range(0, len(lookup)):
+        if lookup[i] == color:
+            return i
 
 
 def colors():
-    pass
+    """List of colors"""
+
+    return [
+        "black",
+        "brown",
+        "red",
+        "orange",
+        "yellow",
+        "green",
+        "blue",
+        "violet",
+        "grey",
+        "white",
+    ]
