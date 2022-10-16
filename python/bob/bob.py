9c3485d5376188862468cc1412066408ed5cba69
Stefhen Hovland <stefhen.hovland@gmail.com>
2022-10-16 17:23:52 -0400

    wip

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,19 +1,36 @@
+"""
+Bob answers 'Sure.' if you ask him a question, such as "How are you?".
+
+He answers 'Whoa, chill out!' if you YELL AT HIM (in all capitals).
+
+He answers 'Calm down, I know what I'm doing!' if you yell a question at him.
+
+He says 'Fine. Be that way!' if you address him without actually saying anything.
+
+He answers 'Whatever.' to anything else.
+"""
+
 def response(hey_bob):
     hey_bob = hey_bob.strip()
 
-
-    if not any(c.isalpha() for c in hey_bob):
-        return "Fine. Be that way!" 
-    
-    if hey_bob == '':
-        return "Whatever."
-    
+    # yelling question
+    # Calm down, I know what I'm doing!
     if hey_bob.upper() == hey_bob and hey_bob[-1] == '?':
         return "Calm down, I know what I'm doing!"
 
+    # question
+    # Sure.
     if hey_bob[-1] == '?':
-        return "Sure."
+        return 'Sure.'
 
-    if hey_bob.upper() == hey_bob:
+    # not saying anything
+    # Fine. Be that way!
+    if none(c.isalpha() for c in hey_bob):
+        return "Fine. Be that way!"
+
+    # yelling
+    if hey_bob.upper() == hey_bob():
         return "Whoa, chill out!"
 
+    # anything else
+    return "Whatever."
