b970ebd2ab8b23c33ba272970a86d989a353c989
Stefhen Hovland <stefhen.hovland@gmail.com>
2022-10-16 17:29:39 -0400

    working

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -13,24 +13,16 @@ He answers 'Whatever.' to anything else.
 def response(hey_bob):
     hey_bob = hey_bob.strip()
 
-    # yelling question
-    # Calm down, I know what I'm doing!
-    if hey_bob.upper() == hey_bob and hey_bob[-1] == '?':
-        return "Calm down, I know what I'm doing!"
-
-    # question
-    # Sure.
-    if hey_bob[-1] == '?':
-        return 'Sure.'
-
-    # not saying anything
-    # Fine. Be that way!
-    if none(c.isalpha() for c in hey_bob):
+    if not hey_bob:
         return "Fine. Be that way!"
 
-    # yelling
-    if hey_bob.upper() == hey_bob():
+    if hey_bob.isupper() and hey_bob.endswith('?'):
+        return "Calm down, I know what I'm doing!"
+    
+    if hey_bob.isupper():
         return "Whoa, chill out!"
+    
+    if hey_bob.endswith('?'):
+        return "Sure."
 
-    # anything else
     return "Whatever."
