067bd5b523a34302b26bccbaea2486f68bbdc6d7
Stefhen Hovland <stefhen.hovland@gmail.com>
2022-10-16 23:23:00 -0400

    wip

--- ocaml/difference-of-squares/difference_of_squares.ml
+++ ocaml/difference-of-squares/difference_of_squares.ml
@@ -1,8 +1,21 @@
 let square_of_sum _ = 
-    failwith "'square_of_sum' is missing"
+    apply map
 
 let sum_of_squares _ =
     failwith "'sum_of_squares' is missing"
 
 let difference_of_squares _ =
     failwith "'difference_of_squares' is missing"
+
+
+
+(*(defn sum-of-squares [n] *)
+(*  (apply + (map #(1* % %) (range 1 (+ n 1))))) *)
+
+(*(defn square-of-sum [n] *)
+(*  (1* *)
+(*    (apply + (map #(+ %) (range 1 (+ n 1)))) *)
+(*    (apply + (map #(+ %) (range 1 (+ n 1)))))) *)
+
+(*(println (- (square-of-sum 100) (sum-of-squares 100))) *)
+
