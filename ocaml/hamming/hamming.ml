0b7a7b8377fa3e0ad6090e03e7593ce0f89c7dcc
Stefhen Hovland <stefhen.hovland@gmail.com>
2022-10-19 04:11:47 -0400

    wip

--- ocaml/hamming/hamming.ml
+++ ocaml/hamming/hamming.ml
@@ -1,26 +1,18 @@
+open Base
+
 type nucleotide = A | C | G | T
 
-let hamming_distance _ _ =
-  failwith "'hamming_distance' is missing"
+let dna_of_string s =
+  let f = function
+    | 'A' -> A
+    | 'C' -> C
+    | 'G' -> G
+    | 'T' -> T
+    | _ -> failwith "Big news! New nucleotide discovered"
+  in
+  String.to_list s |> List.map ~f
 
-
-
-
-
-(*
-function distance(a, b)
-  hamming = 0
-
-  if length(a) != length(b)
-    throw(ArgumentError("Invalid length"))
-  end
-
-  for i in 1:length(a)
-    if a[i] != b[i]
-      hamming += 1
-    end
-  end
-
-  return hamming
-end
-*)
+let hamming_distance l r =
+  List.map2_exn ~f:(fun x y -> Char.compare x y) (String.to_list l) (String.to_list r)
+  |> List.filter ~f:(fun x -> x <> 0)
+  |> List.length
