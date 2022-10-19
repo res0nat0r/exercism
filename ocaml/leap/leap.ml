ed11d1e5415091132aaac33b4c4a41f9d444979a
Stefhen Hovland <stefhen.hovland@gmail.com>
2022-10-19 02:33:52 -0400

    ocaml/leap (#69)

---
+++ ocaml/leap/leap.ml
@@ -0,0 +1,16 @@
+(*
+Given a year, report if it is a leap year.
+
+The tricky thing here is that a leap year in the Gregorian calendar occurs:
+
+on every year that is evenly divisible by 4
+  except every year that is evenly divisible by 100
+    unless the year is also evenly divisible by 400
+
+    For example, 1997 is not a leap year, but 1996 is. 1900 is not a leap year, but 2000 is.
+*)
+
+open Base
+
+let leap_year year =
+  if (year % 4 = 0 && year % 100 <> 0) || year % 400 = 0 then true else false
