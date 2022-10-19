4367fc31ea2cedc677a14717e72cc4ac27e129b0
Stefhen Hovland <stefhen.hovland@gmail.com>
2022-10-19 01:10:09 -0400

    ocamlformat

--- ocaml/difference-of-squares/difference_of_squares.ml
+++ ocaml/difference-of-squares/difference_of_squares.ml
@@ -12,18 +12,12 @@ numbers and the sum of the squares of the first ten natural numbers is 3025 -
 
 open Base
 
-let sum l =
-  List.fold ~init:0 ~f:(+) l
+let sum l = List.fold ~init:0 ~f:( + ) l
+let rec range a b = if a > b then [] else a :: range (a + 1) b
 
-let rec range a b =
-  if a > b then []
-  else a :: range (a + 1 ) b
-let  square_of_sum n =
+let square_of_sum n =
   let total = range 1 n |> sum in
   total * total
 
-let sum_of_squares n =
-  range 1 n |> List.map ~f:(fun x -> x * x) |> sum
-
-let difference_of_squares n =
-  square_of_sum n - sum_of_squares n
+let sum_of_squares n = range 1 n |> List.map ~f:(fun x -> x * x) |> sum
+let difference_of_squares n = square_of_sum n - sum_of_squares n
