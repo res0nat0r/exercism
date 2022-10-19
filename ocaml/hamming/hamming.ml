4aea0c85784d8bba4b35df94ea13d298e1993d1c
Stefhen Hovland <stefhen.hovland@gmail.com>
2022-10-19 02:37:33 -0400

    wip

--- ocaml/hamming/hamming.ml
+++ ocaml/hamming/hamming.ml
@@ -2,3 +2,25 @@ type nucleotide = A | C | G | T
 
 let hamming_distance _ _ =
   failwith "'hamming_distance' is missing"
+
+
+
+
+
+(*
+function distance(a, b)
+  hamming = 0
+
+  if length(a) != length(b)
+    throw(ArgumentError("Invalid length"))
+  end
+
+  for i in 1:length(a)
+    if a[i] != b[i]
+      hamming += 1
+    end
+  end
+
+  return hamming
+end
+*)
