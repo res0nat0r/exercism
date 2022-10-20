0ea43b390a7685c4f7580da28a2c335bfa7ba251
Stefhen Hovland <stefhen.hovland@gmail.com>
2022-10-20 03:58:29 -0400

    working

--- ocaml/hamming/hamming.ml
+++ ocaml/hamming/hamming.ml
@@ -1,17 +1,6 @@
 (*
 Calculate the Hamming Distance between two DNA strands.
 
-Your body is made up of cells that contain DNA. Those cells regularly wear out
-and need replacing, which they achieve by dividing into daughter cells. In fact,
-the average human body experiences about 10 quadrillion cell divisions in a
-lifetime!
-
-When cells divide, their DNA replicates too. Sometimes during this process
-mistakes happen and single pieces of DNA get encoded with the incorrect
-information. If we compare two strands of DNA and count the differences between
-them we can see how many mistakes occurred. This is known as the "Hamming
-Distance".
-
 We read DNA using the letters C,A,G and T. Two strands might look like this:
 
 GAGCCTACTAACGGGAT
@@ -25,17 +14,27 @@ open Base
 
 type nucleotide = A | C | G | T
 
-(* let dna_of_string s =
-  let f = function
-    | 'A' -> A
-    | 'C' -> C
-    | 'G' -> G
-    | 'T' -> T
-    | _ -> failwith "Big news! New nucleotide discovered"
-  in
-  String.to_list s |> List.map ~f *)
+let distance d = 
+  match d with 
+  | (A, A) | (C, C) | (G, G) | (T, T) -> 0
+  | _ -> 1
 
-let hamming_distance l r =
-  List.map2_exn ~f:(fun x y -> Char.compare x y) (String.to_list l) (String.to_list r)
-  |> List.filter ~f:(fun x -> x <> 0)
-  |> List.length
+let hamming_distance l r = match (l, r) with
+  | ([], []) -> Ok 0
+  | ([], _) -> Error "left strand must not be empty"
+  | (_, []) -> Error "right strand must not be empty"
+  | (x, y) when List.length x <> List.length y -> Error "left and right strands must be of equal length"
+  | (x,y)  -> Ok (List.zip_exn x y |> List.map ~f:distance |> List.reduce_exn ~f:(+))
+
+(* let rec list_max l =
+  match l with
+  | [] -> None
+  | h :: t -> begin
+    match list_max t with
+    | None -> Some h
+    | Some m -> Some (max h m)
+  end *)
+
+
+
+  (*  let s = List.zip_exn ~f:distance |> List.reduce ~f:(+) function *)
