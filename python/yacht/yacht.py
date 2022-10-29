4cae59c0f221932130f6a3e2cb1296b352121d0e
Stefhen Hovland <stefhen.hovland@gmail.com>
2022-10-29 11:57:52 -0500

    test

--- python/yacht/yacht.py
+++ python/yacht/yacht.py
@@ -1,6 +1,6 @@
 """
 The dice game Yacht is from the same family as Poker Dice, Generala and
-particularly Yahtzee, of which it is a precursor. In the game, five dice are
+PARticularly Yahtzee, of which it is a precursor. In the game, five dice are
 rolled and the result can be entered in any of twelve categories. The score of a
 throw of the dice depends on category chosen.
 """
