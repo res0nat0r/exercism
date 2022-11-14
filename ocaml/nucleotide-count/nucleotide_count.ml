069b391549ea2f7ae61993cf386f735336be8b0f
Stefhen Hovland <stefhen.hovland@gmail.com>
2022-11-14 17:35:26 -0600

    update

--- ocaml/nucleotide-count/nucleotide_count.ml
+++ ocaml/nucleotide-count/nucleotide_count.ml
@@ -7,20 +7,6 @@ For example:
 
 "GATTACA" -> 'A': 3, 'C': 1, 'G': 1, 'T': 2
 "INVALID" -> error
-
-
-val count_nucleotide : string -> char -> (int, char) Result.t
-
-val count_nucleotides : string -> (int Map.M(Char).t, char) Result.t
-
-
-
-   begin
-      let exp = Ok ((Map.of_alist_exn (module Char)) [('A', 1); ('C', 2); ('G', 3); ('T', 4)])
-      in amre exp (NC.count_nucleotides "CGTATGTCTG")
-    end;
-  ]
-
 *)
 
 open Base
@@ -29,7 +15,5 @@ let empty = Map.empty (module Char)
 
 let count_nucleotide s c = Ok (String.to_list s |> List.count ~f:(fun f -> phys_equal f c))
 
-
-(*   val count_nucleotides : string -> (int, 'a) result list *)
 let count_nucleotides s =
   List.map ~f:(fun f -> count_nucleotide s f) ['A'; 'C'; 'G'; 'T']
