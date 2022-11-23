fcf7a4336096dbf8fa7a6c211c75407f7640b8b5
Stefhen Hovland <stefhen.hovland@gmail.com>
2022-11-23 02:30:59 -0600

    wip

--- crystal/acronym/src/acronym.cr
+++ crystal/acronym/src/acronym.cr
@@ -2,6 +2,6 @@ module Acronym
   extend self
 
   def abbreviate(s)
-    return s.split(/[ _]/).map { |w| w.split(//).first }.join
+    return s.split(/[ _]/).map { |w| w.split(//).first }.join.upcase
   end
 end
