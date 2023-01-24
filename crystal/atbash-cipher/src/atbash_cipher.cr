c234d6aae1acd0b81c65469ada6b70d6850b648c
Stefhen Hovland <stefhen.hovland@gmail.com>
2023-01-24 17:05:07 -0600

    wip

--- crystal/atbash-cipher/src/atbash_cipher.cr
+++ crystal/atbash-cipher/src/atbash_cipher.cr
@@ -1,8 +1,79 @@
 module AtbashCipher
-  # Write your code for the 'Atbash Cipher' exercise in this file.
+  ENCODE = {
+    "a" => "z",
+    "b" => "y",
+    "c" => "x",
+    "d" => "w",
+    "e" => "v",
+    "f" => "u",
+    "g" => "t",
+    "h" => "s",
+    "i" => "r",
+    "j" => "q",
+    "k" => "p",
+    "l" => "o",
+    "m" => "n",
+    "n" => "m",
+    "o" => "l",
+    "p" => "k",
+    "q" => "j",
+    "r" => "i",
+    "s" => "h",
+    "t" => "g",
+    "u" => "f",
+    "v" => "e",
+    "w" => "d",
+    "x" => "c",
+    "y" => "b",
+    "z" => "a",
+  }
+
+  DECODE = {
+    "z" => "a",
+    "y" => "b",
+    "x" => "c",
+    "w" => "d",
+    "v" => "e",
+    "u" => "f",
+    "t" => "g",
+    "s" => "h",
+    "r" => "i",
+    "q" => "j",
+    "p" => "k",
+    "o" => "l",
+    "n" => "m",
+    "m" => "n",
+    "l" => "o",
+    "k" => "p",
+    "j" => "q",
+    "i" => "r",
+    "h" => "s",
+    "g" => "t",
+    "f" => "u",
+    "e" => "v",
+    "d" => "w",
+    "c" => "x",
+    "b" => "y",
+    "a" => "z",
+  }
+
   def self.encode(message : String) : String
+    encoded = [] of String
+
+    message.split(//).each do |m|
+      encoded.push(ENCODE[m])
+    end
+
+    return encoded.join
   end
 
   def self.decode(message : String) : String
+    decoded = [] of String
+
+    message.split(//).each do |m|
+      decoded.push(DECODE[m])
+    end
+
+    return decoded.join
   end
 end
