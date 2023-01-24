8d63354f855894b12bb6e1526bb0f209645362da
Stefhen Hovland <stefhen.hovland@gmail.com>
2023-01-24 13:15:31 -0600

    crystal/anagram (#82)

---
+++ crystal/anagram/src/anagram.cr
@@ -0,0 +1,22 @@
+module Anagram
+  def self.find(subject : String, candidates : Array(String)) : Array(String)
+    s = [] of Char
+    matches = [] of String
+
+    s = subject.split(//).map { |c| c.downcase }.sort.join
+
+    candidates.each do |candidate|
+      if subject.downcase == candidate.downcase
+        next
+      end
+
+      c = candidate.split(//).map { |c| c.downcase }.sort.join
+
+      if s == c
+        matches.push(candidate)
+      end
+    end
+
+    return matches
+  end
+end
