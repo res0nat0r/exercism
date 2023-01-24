76f4bcc7b905edc696c4f378efdff5a1d4b9315b
Stefhen Hovland <stefhen.hovland@gmail.com>
2023-01-24 16:45:43 -0600

    crystal/atbash-cipher

---
+++ crystal/atbash-cipher/src/atbash_cipher.cr
@@ -0,0 +1,8 @@
+module AtbashCipher
+  # Write your code for the 'Atbash Cipher' exercise in this file.
+  def self.encode(message : String) : String
+  end
+
+  def self.decode(message : String) : String
+  end
+end
