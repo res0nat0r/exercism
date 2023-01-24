709f71ef969082cf8c09fd022d396600fa436476
Stefhen Hovland <stefhen.hovland@gmail.com>
2023-01-24 16:39:15 -0600

    wip

--- crystal/armstrong-numbers/src/armstrong_numbers.cr
+++ crystal/armstrong-numbers/src/armstrong_numbers.cr
@@ -1,9 +1,10 @@
 module ArmstrongNumbers
   def self.armstrong_number?(input : Number) : Bool
     sum = 0
+    length = input.to_s.split(//).size
 
-    split_number(input).each_with_index do |n, i|
-      sum += n.to_i ** (i + 1)
+    split_number(input).each do |n|
+      sum += n.to_i ** length
     end
 
     if sum == input
@@ -17,11 +18,3 @@ module ArmstrongNumbers
     return input.to_s.split(//)
   end
 end
-
-# For example:
-
-# 9 is an Armstrong number, because 9 = 9^1 = 9
-# 10 is not an Armstrong number, because 10 != 1^2 + 0^2 = 1
-# 153 is an Armstrong number, because: 153 = 1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153
-# 154 is not an Armstrong number, because: 154 != 1^3 + 5^3 + 4^3 = 1 + 125 + 64 = 190
-# Write some code to determine whether a number is an Armstrong number.
