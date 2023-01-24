817840ef8231f14f039e56599a6383c46a283e98
Stefhen Hovland <stefhen.hovland@gmail.com>
2023-01-23 22:08:10 -0600

    wip

--- crystal/anagram/src/anagram.cr
+++ crystal/anagram/src/anagram.cr
@@ -1,5 +1,22 @@
 module Anagram
   def self.find(subject : String, candidates : Array(String)) : Array(String)
-    # Write your code for the 'Anagram' exercise in this file.
+    s = [] of Char
+    matches = [] of String
+
+    subject.each_char do |c|
+      s.push(c.downcase)
+    end
+
+    s.sort.join
+
+    candidates.each do |c|
+      if s == c.downcase
+        matches.push(c)
+      end
+    end
+
+    return matches
+
+
   end
 end
