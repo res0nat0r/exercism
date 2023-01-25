59fb63c15ef1668e51336053333b17cc55e76bfb
Stefhen Hovland <stefhen.hovland@gmail.com>
2023-01-25 14:32:10 -0600

    working

--- crystal/atbash-cipher/src/atbash_cipher.cr
+++ crystal/atbash-cipher/src/atbash_cipher.cr
@@ -1,61 +1,5 @@
 module AtbashCipher
-  ENCODE = {
-    'a' => 'z',
-    'b' => 'y',
-    'c' => 'x',
-    'd' => 'w',
-    'e' => 'v',
-    'f' => 'u',
-    'g' => 't',
-    'h' => 's',
-    'i' => 'r',
-    'j' => 'q',
-    'k' => 'p',
-    'l' => 'o',
-    'm' => 'n',
-    'n' => 'm',
-    'o' => 'l',
-    'p' => 'k',
-    'q' => 'j',
-    'r' => 'i',
-    's' => 'h',
-    't' => 'g',
-    'u' => 'f',
-    'v' => 'e',
-    'w' => 'd',
-    'x' => 'c',
-    'y' => 'b',
-    'z' => 'a',
-  }
-
-  DECODE = {
-    'z' => 'a',
-    'y' => 'b',
-    'x' => 'c',
-    'w' => 'd',
-    'v' => 'e',
-    'u' => 'f',
-    't' => 'g',
-    's' => 'h',
-    'r' => 'i',
-    'q' => 'j',
-    'p' => 'k',
-    'o' => 'l',
-    'n' => 'm',
-    'm' => 'n',
-    'l' => 'o',
-    'k' => 'p',
-    'j' => 'q',
-    'i' => 'r',
-    'h' => 's',
-    'g' => 't',
-    'f' => 'u',
-    'e' => 'v',
-    'd' => 'w',
-    'c' => 'x',
-    'b' => 'y',
-    'a' => 'z',
-  }
+  CIPHER = Hash.zip(('a'..'z').to_a, ('a'..'z').to_a.reverse)
 
   def self.encode(message : String) : String
     encoded = [] of Char
@@ -65,11 +9,11 @@ module AtbashCipher
       if m.number?
         encoded.push(m)
       else
-        encoded.push(ENCODE[m.downcase])
+        encoded.push(CIPHER[m.downcase])
       end
     end
 
-    return encoded.join.chars.map{|c| c}.in_groups_of(5).map(&.join).join(" ")
+    return encoded.join.chars.map { |c| c }.in_groups_of(5).map(&.join).join(" ")
   end
 
   def self.decode(message : String) : String
@@ -80,10 +24,10 @@ module AtbashCipher
       if m.number?
         decoded.push(m)
       else
-        decoded.push(DECODE[m.downcase])
+        decoded.push(CIPHER.invert[m.downcase])
       end
     end
 
-    return decoded.join.chars.map{|c| c}.join
+    return decoded.join.chars.map { |c| c }.join
   end
 end
