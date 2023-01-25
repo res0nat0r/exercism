389be79d3cd022a3bb1f7713cb61b4738ad69f38
Stefhen Hovland <stefhen.hovland@gmail.com>
2023-01-25 02:03:36 -0600

    wip

--- crystal/atbash-cipher/src/atbash_cipher.cr
+++ crystal/atbash-cipher/src/atbash_cipher.cr
@@ -1,89 +1,89 @@
 module AtbashCipher
   ENCODE = {
-    "a" => "z",
-    "b" => "y",
-    "c" => "x",
-    "d" => "w",
-    "e" => "v",
-    "f" => "u",
-    "g" => "t",
-    "h" => "s",
-    "i" => "r",
-    "j" => "q",
-    "k" => "p",
-    "l" => "o",
-    "m" => "n",
-    "n" => "m",
-    "o" => "l",
-    "p" => "k",
-    "q" => "j",
-    "r" => "i",
-    "s" => "h",
-    "t" => "g",
-    "u" => "f",
-    "v" => "e",
-    "w" => "d",
-    "x" => "c",
-    "y" => "b",
-    "z" => "a",
+    'a' => 'z',
+    'b' => 'y',
+    'c' => 'x',
+    'd' => 'w',
+    'e' => 'v',
+    'f' => 'u',
+    'g' => 't',
+    'h' => 's',
+    'i' => 'r',
+    'j' => 'q',
+    'k' => 'p',
+    'l' => 'o',
+    'm' => 'n',
+    'n' => 'm',
+    'o' => 'l',
+    'p' => 'k',
+    'q' => 'j',
+    'r' => 'i',
+    's' => 'h',
+    't' => 'g',
+    'u' => 'f',
+    'v' => 'e',
+    'w' => 'd',
+    'x' => 'c',
+    'y' => 'b',
+    'z' => 'a',
   }
 
   DECODE = {
-    "z" => "a",
-    "y" => "b",
-    "x" => "c",
-    "w" => "d",
-    "v" => "e",
-    "u" => "f",
-    "t" => "g",
-    "s" => "h",
-    "r" => "i",
-    "q" => "j",
-    "p" => "k",
-    "o" => "l",
-    "n" => "m",
-    "m" => "n",
-    "l" => "o",
-    "k" => "p",
-    "j" => "q",
-    "i" => "r",
-    "h" => "s",
-    "g" => "t",
-    "f" => "u",
-    "e" => "v",
-    "d" => "w",
-    "c" => "x",
-    "b" => "y",
-    "a" => "z",
+    'z' => 'a',
+    'y' => 'b',
+    'x' => 'c',
+    'w' => 'd',
+    'v' => 'e',
+    'u' => 'f',
+    't' => 'g',
+    's' => 'h',
+    'r' => 'i',
+    'q' => 'j',
+    'p' => 'k',
+    'o' => 'l',
+    'n' => 'm',
+    'm' => 'n',
+    'l' => 'o',
+    'k' => 'p',
+    'j' => 'q',
+    'i' => 'r',
+    'h' => 's',
+    'g' => 't',
+    'f' => 'u',
+    'e' => 'v',
+    'd' => 'w',
+    'c' => 'x',
+    'b' => 'y',
+    'a' => 'z',
   }
 
   def self.encode(message : String) : String
-    encoded = [] of String
-    message = message.chars.select { |c| c.ascii_alphanumeric? }.join
+    encoded = [] of Char
+    message = message.chars.select { |c| c.ascii_alphanumeric? }
 
-    message.split(//).each do |m|
-      if m.char.number?
-        decoded.push(m)
+    message.each do |m|
+      if m.number?
+        encoded.push(m)
       else
         encoded.push(ENCODE[m.downcase])
       end
     end
 
-    return encoded.join
+    return encoded.join.chars.map{|c| c}.in_groups_of(5).map(&.join).join(" ")
   end
 
   def self.decode(message : String) : String
-    decoded = [] of String
-    message = message.chars.select { |c| c.ascii_alphanumeric? }.join
+    decoded = [] of Char
+    message = message.chars.select { |c| c.ascii_alphanumeric? }
 
-    message.split(//).each do |m|
-      if m.char.number?
+    message.each do |m|
+      if m.number?
         decoded.push(m)
       else
         decoded.push(DECODE[m.downcase])
       end
     end
 
-    return decoded.join
+    return decoded.join.chars.map{|c| c}.in_groups_of(5).map(&.join).join(" ")
   end
 end
