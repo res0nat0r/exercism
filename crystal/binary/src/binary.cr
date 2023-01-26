f813f8dd9be7d203b358255a6328c480b092df70
Stefhen Hovland <stefhen.hovland@gmail.com>
2023-01-25 21:50:36 -0600

    working

--- crystal/binary/src/binary.cr
+++ crystal/binary/src/binary.cr
@@ -1,1 +1,11 @@
-# Please implement your solution to binary in this file
+module Binary
+  def self.to_decimal(number : String) : Int
+    sum = 0
+
+    number.chars.each_with_index do |n, i|
+      sum += n.to_i * (2 ** (number.size - i - 1))
+    end
+
+    return sum
+  end
+end
