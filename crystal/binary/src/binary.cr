a7eb219d17aefe86f852810b2f344d5e11147624
Stefhen Hovland <stefhen.hovland@gmail.com>
2023-01-25 22:14:46 -0600

    working

--- crystal/binary/src/binary.cr
+++ crystal/binary/src/binary.cr
@@ -3,7 +3,11 @@ module Binary
     sum = 0
 
     number.chars.each_with_index do |n, i|
-      sum += n.to_i * (2 ** (number.size - i - 1))
+      if [0, 1].includes? n.to_i
+        sum += n.to_i * (2 ** (number.size - i - 1))
+      else
+        raise ArgumentError.new("Invalid Digit")
+      end
     end
 
     return sum
