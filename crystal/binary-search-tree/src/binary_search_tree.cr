d9aca38466e5ab136e83f4fc52f9538ba00f8410
Stefhen Hovland <stefhen.hovland@gmail.com>
2023-01-31 18:40:06 -0600

    wip

--- crystal/binary-search-tree/src/binary_search_tree.cr
+++ crystal/binary-search-tree/src/binary_search_tree.cr
@@ -3,6 +3,8 @@ class Node
 
   def initialize(value : Int32)
     @value = value
+    @left = 0
+    @right = 0
   end
 
   def value
@@ -11,5 +13,17 @@ class Node
 
   def each
     yield @value
+  end
+
+  def insert(value : Int32)
+    @value
+  end
+
+  def left(new_value : Int32)
+    if new_value < @value
+      @left = new_value
+    else
+      @left = nil
+    end
   end
 end
