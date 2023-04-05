73b39e7abc65251d3b3795bbb2545ddf36e7f92f
Stefhen Hovland <stefhen.hovland@gmail.com>
2023-04-05 03:51:38 -0500

    ruby/log-line-parser (#92)

---
+++ ruby/log-line-parser/log_line_parser.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class LogLineParser
+  def initialize(line)
+    @line = line
+  end
+
+  def message
+    @line.split(':').last.strip
+  end
+
+  def log_level
+    @line.split(':').first.match(/.(.*)./)[1].downcase
+  end
+
+  def reformat
+    "#{message} (#{log_level})"
+  end
+end
