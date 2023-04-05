e623e6a78dcdf97aecbf6678ac83379faac2f7bb
Stefhen Hovland <stefhen.hovland@gmail.com>
2023-04-05 03:04:20 -0500

    ruby/amusement-park (#88)

---
+++ ruby/amusement-park/attendee.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class Attendee
+  attr_reader :height, :pass_id
+
+  def initialize(height)
+    @height = height
+  end
+
+  def issue_pass!(pass_id)
+    @pass_id = pass_id
+    @pass_id
+  end
+
+  def revoke_pass!
+    @pass_id = nil
+  end
+end
