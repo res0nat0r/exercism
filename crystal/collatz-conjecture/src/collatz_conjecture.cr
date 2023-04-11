d787a77f1c5f36acb3b62c7b540134a636df4d55
Stefhen Hovland <stefhen.hovland@gmail.com>
2023-04-11 02:42:28 -0500

    wip

--- crystal/collatz-conjecture/src/collatz_conjecture.cr
+++ crystal/collatz-conjecture/src/collatz_conjecture.cr
@@ -1,5 +1,22 @@
+# The Collatz Conjecture or 3x+1 problem can be summarized as follows:
+# 
+# Take any positive integer n. If n is even, divide n by 2 to get n / 2. If n is
+# odd, multiply n by 3 and add 1 to get 3n + 1. Repeat the process indefinitely.
+# The conjecture states that no matter which number you start with, you will
+# always reach 1 eventually.
+# 
+# Given a number n, return the number of steps required to reach 1.
+
 module CollatzConjecture
   def self.steps(input : Number) : Number
-    # Write your code for the 'Collatz Conjecture' exercise in this file.
+    counter = 1
+
+    if input == 1
+      return counter
+    end
+
+    if iseven(input)
+      ...
+      
   end
 end
