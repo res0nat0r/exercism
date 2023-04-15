06b138203477c2108e7484a4200b3b911b54d924
Stefhen Hovland <stefhen.hovland@gmail.com>
2023-04-14 20:07:47 -0500

    working

--- crystal/difference-of-squares/src/difference_of_squares.cr
+++ crystal/difference-of-squares/src/difference_of_squares.cr
@@ -27,12 +27,22 @@
 
 module Squares(T)
   def self.sum_of_squares(n : T)
-    # wip
+    sum = 0
+
+    (1..n).each do |i|
+      sum += i * i
+    end
+
+    return sum
   end
 
   def self.square_of_sum(n : T)
     s = (1..n).to_a.sum
-    
-    return  s * s
+
+    return s * s
+  end
+
+  def self.difference_of_squares(n : T)
+    return square_of_sum(n) - sum_of_squares(n)
   end
 end
