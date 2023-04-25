c8abcc86a8d4ac9053bbbf029893543cb317f6bf
Stefhen Hovland <stefhen.hovland@gmail.com>
2023-04-25 12:15:22 -0500

    update

--- julia/atbash-cipher/atbash-cipher.jl
+++ julia/atbash-cipher/atbash-cipher.jl
@@ -1,8 +1,20 @@
 function encode(input)
+  input = lowercase(input)
+  plain = "abcdefghijklmnopqrstuvwxyz"
+  cipher = "zyxwvutsrqponmlkjihgfedcba"
+  output = ""
+  output_cipher = ""
+  lookup = Dict(zip(plain, cipher))
+  re = r"[a-z]"
+
+  for m in eachmatch(re, input)
+    output = string(output, m.match)
+  end
+
+  for i in output
+    output_cipher = string(output_cipher, lookup[i])
+  end
+
+  return output_cipher
 
 end
-
-function decode(input)
-
-end
-
