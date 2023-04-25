421189f60d2afdc7748bad691fa31d10704161f2
Stefhen Hovland <stefhen.hovland@gmail.com>
2023-04-25 12:17:10 -0500

    formatting

--- julia/atbash-cipher/runtests.jl
+++ julia/atbash-cipher/runtests.jl
@@ -10,15 +10,17 @@ include("atbash-cipher.jl")
     @test encode("mindblowingly") == "nrmwy oldrm tob"
     @test encode("Testing,1 2 3, testing.") == "gvhgr mt123 gvhgr mt"
     @test encode("Truth is fiction.") == "gifgs rhurx grlm"
-    @test encode("The quick brown fox jumps over the lazy dog.") == "gsvjf rxpyi ldmul cqfnk hlevi gsvoz abwlt"
+    @test encode("The quick brown fox jumps over the lazy dog.") ==
+          "gsvjf rxpyi ldmul cqfnk hlevi gsvoz abwlt"
 end
 
 @testset "decoding from atbash to English" begin
     @test decode("vcvix rhn") == "exercism"
-    @test decode("zmlyh gzxov rhlug vmzhg vkkrm thglm v") == "anobstacleisoftenasteppingstone"
+    @test decode("zmlyh gzxov rhlug vmzhg vkkrm thglm v") ==
+          "anobstacleisoftenasteppingstone"
     @test decode("gvhgr mt123 gvhgr mt") == "testing123testing"
-    @test decode("gsvjf rxpyi ldmul cqfnk hlevi gsvoz abwlt") == "thequickbrownfoxjumpsoverthelazydog"
+    @test decode("gsvjf rxpyi ldmul cqfnk hlevi gsvoz abwlt") ==
+          "thequickbrownfoxjumpsoverthelazydog"
     @test decode("vc vix    r hn") == "exercism"
     @test decode("zmlyhgzxovrhlugvmzhgvkkrmthglmv") == "anobstacleisoftenasteppingstone"
 end
-
