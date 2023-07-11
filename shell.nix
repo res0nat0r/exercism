c2a15c818586a35a1eab60598a0802a6ce88fe66
Stefhen Hovland <stefhen.hovland@gmail.com>
2023-07-10 22:25:19 -0500

    feature/devcontainer (#117)
    * nix devenv
    
    * devcontainer using features

---
+++ shell.nix
@@ -0,0 +1,24 @@
+{ pkgs ? import <nixpkgs> { } }:
+
+pkgs.mkShell {
+  nativeBuildInputs = with pkgs; [
+    ballerina
+    clojure
+    leiningen
+    elixir
+    ocaml
+    crystal
+    julia
+    python310Full
+    ruby_3_2
+    cargo
+    rustc
+    haskell.compiler.native-bignum.ghc928
+    stack
+
+    exercism
+    diff-so-fancy
+    tig
+    vim
+  ];
+}
