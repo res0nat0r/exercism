a596a049c111dd312fa554c304c7fa13f7b80ee9
Stefhen Hovland <stefhen.hovland@gmail.com>
2023-08-09 15:57:14 -0500

    wip

--- haskell/collatz-conjecture/src/CollatzConjecture.hs
+++ haskell/collatz-conjecture/src/CollatzConjecture.hs
@@ -1,7 +1,14 @@
 module CollatzConjecture (collatz) where
 
 collatz :: Integer -> Maybe Integer
-collatz n = error "You need to implement this function."
+collatz n = collatz' n 0
+
+collatz' :: Integer -> Integer -> Maybe Integer
+collatz' n count
+  | n <= 0 = Nothing
+  | n == 0 = Just count
+  | even n = collatz' (div n 2) (count + 1)
+  | otherwise = collatz' ((3 * n) + 1) (count + 1)
 
 {-
 f(n) = n / 2  for n `mod` 2 == 0
