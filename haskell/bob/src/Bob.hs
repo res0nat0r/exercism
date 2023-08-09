b611eecab90ce9d76ed1d15357a3f00a227c6517
Stefhen Hovland <stefhen.hovland@gmail.com>
2023-08-09 16:12:49 -0500

    wip

--- haskell/bob/src/Bob.hs
+++ haskell/bob/src/Bob.hs
@@ -1,4 +1,13 @@
 module Bob (responseFor) where
 
 responseFor :: String -> String
-responseFor xs = error "You need to implement this function."
+responseFor xs 
+
+
+{-
+"Sure." This is his response if you ask him a question, such as "How are you?" The convention used for questions is that it ends with a question mark.
+"Whoa, chill out!" This is his answer if you YELL AT HIM. The convention used for yelling is ALL CAPITAL LETTERS.
+"Calm down, I know what I'm doing!" This is what he says if you yell a question at him.
+"Fine. Be that way!" This is how he responds to silence. The convention used for silence is nothing, or various combinations of whitespace characters.
+"Whatever." This is what he answers to anything else.
+-}
