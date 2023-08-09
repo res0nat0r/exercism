b75036847acc08426e9ef8ede3161d5295428003
Stefhen Hovland <stefhen.hovland@gmail.com>
2023-08-08 19:34:59 -0500

    update

--- haskell/leap/src/LeapYear.hs
+++ haskell/leap/src/LeapYear.hs
@@ -1,6 +1,6 @@
 module LeapYear (isLeapYear) where
 
 isLeapYear :: Integer -> Bool
-isLeapYear year 
+isLeapYear year
   | year `mod` 4 == 0 && year `mod` 100 /= 0 || year `mod` 400 == 0 = True
   | otherwise = False
