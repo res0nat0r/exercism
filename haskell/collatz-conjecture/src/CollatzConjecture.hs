d3a836945e94b85638235d2b9805a2c530a2b65f
Stefhen Hovland <stefhen.hovland@gmail.com>
2023-08-09 16:02:52 -0500

    working

--- haskell/collatz-conjecture/src/CollatzConjecture.hs
+++ haskell/collatz-conjecture/src/CollatzConjecture.hs
@@ -5,12 +5,7 @@ collatz n = collatz' n 0
 
 collatz' :: Integer -> Integer -> Maybe Integer
 collatz' n count
+  | n == 1 = Just count
   | n <= 0 = Nothing
-  | n == 0 = Just count
   | even n = collatz' (div n 2) (count + 1)
   | otherwise = collatz' ((3 * n) + 1) (count + 1)
-
-{-
-f(n) = n / 2  for n `mod` 2 == 0
-f(n) = 3n + 1 for n `mod 2 /= 0
--}
