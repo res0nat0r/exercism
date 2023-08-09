7a53955678a0765b9955fce52b68c13d5df5dace
Stefhen Hovland <stefhen.hovland@gmail.com>
2023-08-08 23:51:52 -0500

    wip

--- haskell/collatz-conjecture/src/CollatzConjecture.hs
+++ haskell/collatz-conjecture/src/CollatzConjecture.hs
@@ -2,3 +2,8 @@ module CollatzConjecture (collatz) where
 
 collatz :: Integer -> Maybe Integer
 collatz n = error "You need to implement this function."
+
+{-
+f(n) = n / 2  for n `mod` 2 == 0
+f(n) = 3n + 1 for n `mod 2 /= 0
+-}
