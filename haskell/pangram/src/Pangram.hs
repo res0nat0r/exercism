6ecb508187606fe226e0303d354919ccdcc1cfdc
Stefhen Hovland <stefhen.hovland@gmail.com>
2023-08-08 22:45:35 -0500

    wip

--- haskell/pangram/src/Pangram.hs
+++ haskell/pangram/src/Pangram.hs
@@ -1,4 +1,9 @@
 module Pangram (isPangram) where
+import Data.Char
 
 isPangram :: String -> Bool
-isPangram text = error "You need to implement this function."
+isPangram text
+  | text == [] = False
+  | otherwise = all (== True) $ map (\x -> x `elem` alphabet) $ map toLower $ filter isAlpha text
+  where
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
