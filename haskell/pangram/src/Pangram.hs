193c4799c4a624dec45751252ce4b538e545dfcf
Stefhen Hovland <stefhen.hovland@gmail.com>
2023-08-08 23:20:13 -0500

    update

--- haskell/pangram/src/Pangram.hs
+++ haskell/pangram/src/Pangram.hs
@@ -1,12 +1,12 @@
 module Pangram (isPangram) where
-import Data.Char
-import Data.List
+import           Data.Char
+import           Data.List
 
 isPangram :: String -> Bool
 isPangram text
   | null text = False
-  | otherwise = input == 26
+  | otherwise = inputLength == 26
   where
     alphabet = "abcdefghijklmnopqrstuvwxyz"
-    input = length $ map (`elem` alphabet) $ nub $ map toLower $ filter isAscii $ filter isAlpha text
-    
+    inputLength = length $ map (`elem` alphabet) $ nub $ map toLower $ filter isAscii $ filter isAlpha text
+
