e5a24abbf69ca03ab805f52622726e91f0786598
Stefhen Hovland <stefhen.hovland@gmail.com>
2023-08-10 13:57:40 -0500

    containers

--- haskell/rna-transcription/src/DNA.hs
+++ haskell/rna-transcription/src/DNA.hs
@@ -1,4 +1,10 @@
 module DNA (toRNA) where
 
+import Data.Map (Map)
+import qualified Data.Map as Map
+
+rna :: Map String String
+rna = Map.fromList [("G", "C"), ("C", "G"), ("T", "A"), ("A", "U")]
+
 toRNA :: String -> Either Char String
 toRNA xs = error "You need to implement this function."
