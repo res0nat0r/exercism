b3e28272f5f0f982936c5490f1bc278e51ba61a2
Stefhen Hovland <stefhen.hovland@gmail.com>
2023-08-09 23:44:43 -0500

    wip

--- haskell/bob/src/Bob.hs
+++ haskell/bob/src/Bob.hs
@@ -1,7 +1,17 @@
 module Bob (responseFor) where
 
-import Data.Text (Text)
-import qualified Data.Text as T
+import Data.Char
 
-responseFor :: Text -> Text
-responseFor xs = "xxx"
+responseFor :: String -> String
+responseFor xs
+  | isShouting xs && last xs == '?' = "Calm down, I know what I'm doing!"
+  | isShouting xs = "Whoa, chill out!"
+  | last xs == '?' = "Sure."
+  | all (not . isAlpha) xs = "Fine. Be that way!"
+  | otherwise = "Whatever."
+
+-- isShouting :: String -> Bool
+-- isShouting xs = all isUpper $ filter isAlpha xs
+
+isShouting :: String -> Bool
+isShouting xs = all isUpper $ filter isAlpha xs
