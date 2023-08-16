9d3ccc25eff8e55437ffaf5bce711bc0ac0fc473
Stefhen Hovland <stefhen.hovland@gmail.com>
2023-08-16 00:36:12 -0500

    update

--- haskell/rna-transcription/src/DNA.hs
+++ haskell/rna-transcription/src/DNA.hs
@@ -1,24 +1,10 @@
 module DNA (toRNA) where
 
-import Data.Map (Map)
-import qualified Data.Map as Map
-
-rna :: Map String String
-rna = Map.fromList [("G", "C"), ("C", "G"), ("T", "A"), ("A", "U")]
-
 toRNA :: String -> Either Char String
-toRNA xs = lookupRNA xs
-
-lookupRNA :: String -> Either Char String
-lookupRNA dna = case Map.lookup dna rna of
-  Just result -> Right result
-  Nothing -> Left ( head dna)
-
-
-
-  -- Nothing -> Right "NADA"
-  -- Just result -> Left result
-
-
-
--- https://stackoverflow.com/questions/20498501/create-hashtable-in-haskell1
+toRNA = traverse dna
+  where
+    dna 'G' = Right 'C'
+    dna 'C' = Right 'G'
+    dna 'T' = Right 'A'
+    dna 'A' = Right 'U'
+    dna x = Left x
