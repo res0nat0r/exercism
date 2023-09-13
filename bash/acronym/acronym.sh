1f61938ebad75b6bf91efcf89923679e795593ee
Stefhen Hovland <stefhen.hovland@gmail.com>
2023-09-13 14:17:34 -0500

    working

--- bash/acronym/acronym.sh
+++ bash/acronym/acronym.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-for word in $(echo $1 | xargs -0 | tr 'a-z' 'A-Z' | sed 's/[-_\*]/ /g'); do
-  echo -n ${word:0:1}
+for word in $(echo "$1" | xargs -0 | tr '[:lower:]' '[:upper:'] | sed 's/[-_\*]/ /g'); do
+	echo -n "${word:0:1}"
 done
