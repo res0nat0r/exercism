865df8431dca4f1a987974a5535f6b62ebf22ade
Stefhen Hovland <stefhen.hovland@gmail.com>
2023-09-12 21:32:53 -0500

    wip

--- bash/raindrops/raindrops.sh
+++ bash/raindrops/raindrops.sh
@@ -1,24 +1,27 @@
 #!/usr/bin/env bash
 
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+
+# has 3 as a factor, add 'Pling' to the result.
+# has 5 as a factor, add 'Plang' to the result.
+# has 7 as a factor, add 'Plong' to the result.
+# does not have any of 3, 5, or 7 as a factor, the result should be the digits of the number.
+
+
+output=""
+
+let "pling = $1 % 3"
+let "plang = $1 % 5"
+let "plong = $1 % 7"
+
+if [ $pling -eq 0 ]; then
+  output+="Pling"
+elif [ $plang -eq 0 ]; then
+  output+="Plang"
+elif [ $plong -eq 0 ]; then
+  output+="Plong"
+else
+  output=$0
+  fi
+
+echo $output
+
