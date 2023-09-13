5b24f06c31cbaf7035fd4ed3ae0faec365e73bfe
Stefhen Hovland <stefhen.hovland@gmail.com>
2023-09-12 21:33:11 -0500

    fmt

--- bash/raindrops/raindrops.sh
+++ bash/raindrops/raindrops.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
-
 # has 3 as a factor, add 'Pling' to the result.
 # has 5 as a factor, add 'Plang' to the result.
 # has 7 as a factor, add 'Plong' to the result.
 # does not have any of 3, 5, or 7 as a factor, the result should be the digits of the number.
-
 
 output=""
 
@@ -14,14 +12,13 @@ let "plang = $1 % 5"
 let "plong = $1 % 7"
 
 if [ $pling -eq 0 ]; then
-  output+="Pling"
+	output+="Pling"
 elif [ $plang -eq 0 ]; then
-  output+="Plang"
+	output+="Plang"
 elif [ $plong -eq 0 ]; then
-  output+="Plong"
+	output+="Plong"
 else
-  output=$0
-  fi
+	output=$0
+fi
 
 echo $output
-
