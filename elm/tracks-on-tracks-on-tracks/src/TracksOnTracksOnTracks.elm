0aa1760e4fd6bc6a109d099227db0f15996388cf
Stefhen Hovland <stefhen.hovland@gmail.com>
2023-09-13 23:25:00 -0500

    wip

--- elm/tracks-on-tracks-on-tracks/src/TracksOnTracksOnTracks.elm
+++ elm/tracks-on-tracks-on-tracks/src/TracksOnTracksOnTracks.elm
@@ -2,36 +2,34 @@ module TracksOnTracksOnTracks exposing (..)
 
 
 newList : List String
-newList =
-    []
+newList = []
 
 
 existingList : List String
-existingList =
-    [ "Elm", "Clojure", "Haskell" ]
+existingList = ["Elm","Clojure","Haskell"]
 
 
 addLanguage : String -> List String -> List String
-addLanguage language languages =
-    language :: languages
+addLanguage language languages = language :: languages
 
 
 countLanguages : List String -> Int
-countLanguages languages =
-    List.length languages
+countLanguages languages = List.length languages
 
 
 reverseList : List String -> List String
-reverseList languages =
-    List.reverse languages
+reverseList languages = List.reverse languages
 
 
 excitingList : List String -> Bool
-excitingList languages =
-    let
-        (x :: xs) = languages
-        len = List.length languages
-    in
-    if x == "Elm" then True
-    else if len <= 3 then True
-    else False
+excitingList languages = 
+    case languages of
+        "Elm" :: _ -> True
+        _ :: "Elm" :: _ -> True
+        _ -> False
+        
+
+{-
+The first item on the list is Elm.
+Or the second item on the list is Elm and the list contains three or less languages.
+-}
