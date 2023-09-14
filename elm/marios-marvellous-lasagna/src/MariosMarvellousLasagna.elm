e87ecf4f6e5cdb16c1c1a9731c445ed292e43d2d
Stefhen Hovland <stefhen.hovland@gmail.com>
2023-09-14 03:38:36 -0500

    working

--- elm/marios-marvellous-lasagna/src/MariosMarvellousLasagna.elm
+++ elm/marios-marvellous-lasagna/src/MariosMarvellousLasagna.elm
@@ -1,11 +1,14 @@
 module MariosMarvellousLasagna exposing (remainingTimeInMinutes)
 
+
 remainingTimeInMinutes : Int -> Int -> Int
 remainingTimeInMinutes numberOfLayers numberOfMinutes =
     let
+        expectedMinutesInOven : Int
         expectedMinutesInOven =
             40
 
+        preparationTimeInMinutes : Int
         preparationTimeInMinutes =
             2 * numberOfLayers
     in
