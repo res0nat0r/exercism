c071bf9f8086b7238467ed4906f6c3b8b22371fc
Stefhen Hovland <stefhen.hovland@gmail.com>
2023-09-14 20:18:39 -0500

    wip

--- julia/binary-search/binary-search.jl
+++ julia/binary-search/binary-search.jl
@@ -2,22 +2,28 @@
 # enable testing different bonus tasks
 tested_bonus_tasks = (rev = false, by = false, lt = false, multiple_matches = false)
 
-function binarysearch(input, value)
-    binsearch(input, value, left, right)
-end
 
 function binsearch(input, value, left, right)
     while left <= right
         mid = left + Int(floor(((right - 1) /2)))
 
+        println("mid: ", string(mid))
+
         if input[mid] == value
+            println(string("mid: ", string(mid)))
             return mid
         elseif input[mid] < value
+            println(string("left: ", string(left)))
             left = mid + 1
         else
+            println(string("right: ", string(right)))
             right = mid - 1
         end
 
     return -1
     end
+end
+
+function binarysearch(input, value)
+    binsearch(input, value, 1, length(input))
 end
