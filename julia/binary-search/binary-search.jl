b8c5b33ef222c501113a2b67b6bdbb002717be7b
Stefhen Hovland <stefhen.hovland@gmail.com>
2023-09-14 20:05:09 -0500

    wip

--- julia/binary-search/binary-search.jl
+++ julia/binary-search/binary-search.jl
@@ -1,3 +1,23 @@
 # Change any of the following NamedTuple values to true to 
 # enable testing different bonus tasks
 tested_bonus_tasks = (rev = false, by = false, lt = false, multiple_matches = false)
+
+function binarysearch(input, value)
+    binsearch(input, value, left, right)
+end
+
+function binsearch(input, value, left, right)
+    while left <= right
+        mid = left + Int(floor(((right - 1) /2)))
+
+        if input[mid] == value
+            return mid
+        elseif input[mid] < value
+            left = mid + 1
+        else
+            right = mid - 1
+        end
+
+    return -1
+    end
+end
