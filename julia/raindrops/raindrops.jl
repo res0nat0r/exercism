9bfbc8c8d425fb518eeedcd49bc9a2d3881ec499
Stefhen Hovland <stefhen.hovland@gmail.com>
2023-09-18 15:38:06 -0500

    wip

--- julia/raindrops/raindrops.jl
+++ julia/raindrops/raindrops.jl
@@ -1,3 +1,15 @@
 function raindrops(number)
+  result = "" 
 
+  if mod(number, 3) == 0
+    result = result * "Pling"
+  elseif mod(number, 5) == 0
+    result = result * "Plang"
+  elseif mod(number, 7) == 0
+    result = result * "Plong"
+  else
+    result = string(result)
+  end
+
+  return result
 end
