470a207623c07949a644483e1d7fc49df5ffde7f
Stefhen Hovland <stefhen.hovland@gmail.com>
2023-12-06 21:35:03 -0600

    wip

--- python/rotational-cipher/rotational_cipher.py
+++ python/rotational-cipher/rotational_cipher.py
@@ -5,3 +5,11 @@ def rotate(text, key):
     lookup = dict(zip(alphabet, cipher))
 
     return lookup
+
+"""
+ROT5 omg gives trl
+ROT0 c gives c
+ROT26 Cool gives Cool
+ROT13 The quick brown fox jumps over the lazy dog. gives Gur dhvpx oebja sbk whzcf bire gur ynml qbt.
+ROT13 Gur dhvpx oebja sbk whzcf bire gur ynml qbt. gives The quick brown fox jumps over the lazy dog.
+"""
