8f91d01d7d694a12284cfa91565adb159fbc7605
Stefhen Hovland <stefhen.hovland@gmail.com>
2023-12-15 00:13:53 -0600

    feature/nix (#158)

--- shell.nix
+++ shell.nix
@@ -41,6 +41,9 @@ mkShell {
     elmPackages.elm-review
     elmPackages.elm-test
 
+    # fsharp
+    fsharp
+
     # haskell
     haskell.compiler.native-bignum.ghc928
     ihaskell
@@ -53,6 +56,9 @@ mkShell {
 
     # julia
     julia
+
+    # lua
+    lua
 
     # python
     jupyter
