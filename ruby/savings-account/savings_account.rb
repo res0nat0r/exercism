8eaa96977a1818fdfbbe4f8da67896bad58e88da
Stefhen Hovland <stefhen.hovland@gmail.com>
2024-02-06 19:21:28 -0600

    wip

--- ruby/savings-account/savings_account.rb
+++ ruby/savings-account/savings_account.rb
@@ -1,10 +1,24 @@
+# 0.5% for a non-negative balance less than 1000 dollars.
+# 1.621% for a positive balance greater than or equal to 1000 dollars and less than 5000 dollars.
+# 2.475% for a positive balance greater than or equal to 5000 dollars.
+# 3.213% for a negative balance (results in negative interest).
+
 module SavingsAccount
   def self.interest_rate(balance)
-    raise 'Please implement the SavingsAccount.interest_rate method'
+    case balance
+    when balance < 1000 && balance > 0
+      0.5
+    when balance >= 1000 && balance < 5000
+      1.621
+    when balance >= 5000
+      2.475
+    when balance < 0
+      3.213
+    end
   end
 
   def self.annual_balance_update(balance)
-    raise 'Please implement the SavingsAccount.annual_balance_update method'
+    balance += balance * interest_rate(balance)
   end
 
   def self.years_before_desired_balance(current_balance, desired_balance)
