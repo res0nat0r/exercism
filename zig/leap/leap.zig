bfddbad365220d3b7a129ccf9b5b0ec783a29269
Stefhen Hovland <stefhen.hovland@gmail.com>
2024-05-08 12:22:57 -0500

    init

--- zig/leap/leap.zig
+++ zig/leap/leap.zig
@@ -1,4 +1,16 @@
 pub fn isLeapYear(year: u32) bool {
     _ = year;
-    @compileError("please implement the isLeapYear function");
 }
+
+//
+//  A leap year (in the Gregorian calendar) occurs:
+//
+//  • In every year that is evenly divisible by 4.
+//  • Unless the year is evenly divisible by 100, in which case it's only a leap year if the year is also evenly
+//  divisible by 400.
+//
+//  Some examples:
+//
+//  • 1997 was not a leap year as it's not divisible by 4.
+//  • 1900 was not a leap year as it's not divisible by 400.
+//  • 2000 was a leap year!
