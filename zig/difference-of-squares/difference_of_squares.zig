4fbad962fe259257e73e9edaf24cbbd2013095bf
Stefhen Hovland <stefhen.hovland@gmail.com>
2024-05-13 18:08:57 -0500

    wip

--- zig/difference-of-squares/difference_of_squares.zig
+++ zig/difference-of-squares/difference_of_squares.zig
@@ -1,6 +1,5 @@
 pub fn squareOfSum(number: usize) usize {
-    _ = number;
-    @compileError("compute the sum of i from 0 to n then square it");
+
 }
 
 pub fn sumOfSquares(number: usize) usize {
@@ -12,3 +11,18 @@ pub fn differenceOfSquares(number: usize) usize {
     _ = number;
     @compileError("compute the difference between the square of sum and sum of squares");
 }
+
+
+// function square_of_sum(n)
+//     return sum(1:n) * sum(1:n)
+// end
+
+// "Sum the squares of the first `n` positive integers"
+// function sum_of_squares(n)
+//     return sum(map(x -> x * x, 1:n))
+// end
+
+// "Subtract the sum of squares from square of the sum of the first `n` positive ints"
+// function difference(n)
+//     return square_of_sum(n) - sum_of_squares(n)
+// end
