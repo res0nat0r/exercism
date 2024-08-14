6e9b978f435e8ac90674e40ac961cfec5b3d7386
Stefhen Hovland <stefhen.hovland@gmail.com>
2024-08-13 20:57:07 -0500

    rm comments

--- zig/difference-of-squares/difference_of_squares.zig
+++ zig/difference-of-squares/difference_of_squares.zig
@@ -1,7 +1,7 @@
 pub fn squareOfSum(number: usize) usize {
     var sum: usize = 0;
 
-    for (1..number+1) |i| {
+    for (1..number + 1) |i| {
         sum += i;
     }
 
@@ -12,8 +12,8 @@ pub fn sumOfSquares(number: usize) usize {
     var sum: usize = 0;
     var tmp: usize = 0;
 
-    for (1..number+1) |i| {
-        tmp = i*i;
+    for (1..number + 1) |i| {
+        tmp = i * i;
         sum += tmp;
     }
 
@@ -23,17 +23,3 @@ pub fn sumOfSquares(number: usize) usize {
 pub fn differenceOfSquares(number: usize) usize {
     return squareOfSum(number) - sumOfSquares(number);
 }
-
-// function square_of_sum(n)
-//     return sum(1:n) * sum(1:n)
-// end
-
-// "Sum the squares of the first `n` positive integers"
-// function sum_of_squares(n)
-//     return sum(map(x -> x * x, 1:n))
-// end
-
-// "Subtract the sum of squares from square of the sum of the first `n` positive ints"
-// function difference(n)
-//     return square_of_sum(n) - sum_of_squares(n)
-// end
