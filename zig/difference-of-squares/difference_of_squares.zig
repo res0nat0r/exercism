015422f3940642a9ca51497744e0646f92867fc9
Stefhen Hovland <stefhen.hovland@gmail.com>
2024-08-13 20:55:35 -0500

    working

--- zig/difference-of-squares/difference_of_squares.zig
+++ zig/difference-of-squares/difference_of_squares.zig
@@ -1,18 +1,20 @@
 pub fn squareOfSum(number: usize) usize {
     var sum: usize = 0;
 
-    for (number, 1..) |_, i| {
+    for (1..number+1) |i| {
         sum += i;
     }
 
-    return sum;
+    return sum * sum;
 }
 
 pub fn sumOfSquares(number: usize) usize {
     var sum: usize = 0;
+    var tmp: usize = 0;
 
-    for (number, 1..) |_, i| {
-        sum *= i;
+    for (1..number+1) |i| {
+        tmp = i*i;
+        sum += tmp;
     }
 
     return sum;
