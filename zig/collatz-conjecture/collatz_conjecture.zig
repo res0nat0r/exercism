953802c45b8833050d32090e9f3964994efd403c
Stefhen Hovland <stefhen.hovland@gmail.com>
2024-08-13 21:16:18 -0500

    wip

--- zig/collatz-conjecture/collatz_conjecture.zig
+++ zig/collatz-conjecture/collatz_conjecture.zig
@@ -1,6 +1,20 @@
-// Please implement the `ComputationError.IllegalArgument` error.
-
 pub fn steps(number: usize) anyerror!usize {
-    _ = number;
-    @compileError("please implement the steps function");
+    var counter: usize = 0;
+    var tmp: usize = number;
+
+    if (tmp < 1) {
+        return error.DivideByZero;
+    }
+
+    while (tmp != 1) {
+        if (tmp / 2 == 0) {
+            tmp = tmp / 2;
+            counter += 1;
+        } else {
+            tmp = (tmp * 3) + 1;
+            counter += 1;
+        }
+    }
+
+    return counter;
 }
