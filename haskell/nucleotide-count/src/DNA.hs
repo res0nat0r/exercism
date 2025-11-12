3bd6c1866e85a2f529aa3bc6054c09a4562b4093
Stefhen Hovland <stefhen.hovland@gmail.com>
2025-11-12 16:17:41 -0600

    wip

--- haskell/nucleotide-count/src/DNA.hs
+++ haskell/nucleotide-count/src/DNA.hs
@@ -4,13 +4,14 @@ import Data.Map (Map)
 
 data Nucleotide = A | C | G | T deriving (Eq, Ord, Show)
 
-nucleotideCounts :: String -> Either String (Map Nucleotide Int)
-nucleotideCounts = error "todo"
-
 count :: Char -> String -> Int
 count x xs = length $ filter (== x) xs
 
 
+
+
+nucleotideCounts :: String -> Either String (Map Nucleotide Int)
+nucleotideCounts = error "todo"
 
 
 
