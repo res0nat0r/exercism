997d5edaa6174e7348dd3b69406fd8ce30842e1d
Stefhen Hovland <stefhen.hovland@gmail.com>
2025-11-15 19:32:10 -0600

    wip

--- haskell/darts/src/Darts.hs
+++ haskell/darts/src/Darts.hs
@@ -1,4 +1,7 @@
 module Darts (score) where
 
-score :: Float -> Float -> Int
-score x y = error "You need to implement this function."
+-- distance :: Float -> Float -> Float
+-- distance x y = sqrt(x^2 + y^2)
+
+score :: Int -> Int -> Float
+score x y = sqrt (((x :: Float) ^ 2) + ((y :: Float) ^ 2))
