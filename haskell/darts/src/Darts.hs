8e9a6059f0430b14dea2ab571deda39e2b74eadc
Stefhen Hovland <stefhen.hovland@gmail.com>
2025-11-26 18:14:11 -0600

    wip

--- haskell/darts/src/Darts.hs
+++ haskell/darts/src/Darts.hs
@@ -3,5 +3,5 @@ module Darts (score) where
 -- distance :: Float -> Float -> Float
 -- distance x y = sqrt(x^2 + y^2)
 
-score :: Int -> Int -> Float
+--score :: Int -> Int -> Float
 score x y = sqrt (((x :: Float) ^ 2) + ((y :: Float) ^ 2))
